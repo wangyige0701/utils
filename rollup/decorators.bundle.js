8b292e5e8c3043550074463b19d64786d507982b
WangYige <1776686515@qq.com>
2024-09-17 01:09:20 +0800

    打包调整

--- rollup/decorators.bundle.js
+++ rollup/decorators.bundle.js
@@ -2,17 +2,24 @@ import esbuild from 'rollup-plugin-esbuild';
 import dts from 'rollup-plugin-dts';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import alias from '@rollup/plugin-alias';
 import typescript from 'rollup-plugin-typescript2';
 
-import decoratorsBundle from './rollup/decorators.bundle.js';
+const enteries = ['src/decorators/index.ts'];
 
-const enteries = ['src/index.ts'];
+/** @type {import('rollup').Plugin} */
+const path_reset = {
+	name: 'path-reset',
+	renderChunk(code) {
+		return code
+			.replace(/(import.+['"])(@\/(?!decorators).+)(['"];)/g, '$1..$3')
+			.replace(
+				/((?:var|let|const)\s*.+\s*=\s*require\(['"])(@\/(?!decorators).+)(['"]\);)/g,
+				'$1..$3',
+			);
+	},
+};
 
 const plugins = [
-	alias({
-		entries: [{ find: '^node:(.+)$', replacement: '$1' }],
-	}),
 	resolve({
 		preferBuiltins: true,
 		rootDir: 'src',
@@ -22,25 +29,31 @@ const plugins = [
 	esbuild({
 		target: 'node14',
 	}),
+	path_reset,
 ];
 
 /** @type {import('rollup').RollupOptions[]} */
 export default [
+	// 装饰器文件
 	...enteries.map(input => {
 		/** @type {import('rollup').RollupOptions} */
 		const config = {
 			input,
 			output: [
 				{
-					file: input.replace('src/', 'dist/').replace('.ts', '.mjs'),
+					file: input
+						.replace('src/decorators/', 'dist/decorators/')
+						.replace('.ts', '.mjs'),
 					format: 'esm',
 				},
 				{
-					file: input.replace('src/', 'dist/').replace('.ts', '.cjs'),
+					file: input
+						.replace('src/decorators/', 'dist/decorators/')
+						.replace('.ts', '.cjs'),
 					format: 'cjs',
 				},
 			],
-			external: [],
+			external: [/^@\/(?!decorators).+/],
 			plugins,
 		};
 		return config;
@@ -52,27 +65,26 @@ export default [
 			output: [
 				{
 					file: input
-						.replace('src/', 'dist/')
+						.replace('src/decorators/', 'dist/decorators/')
 						.replace('.ts', '.d.mts'),
 					format: 'esm',
 				},
 				{
 					file: input
-						.replace('src/', 'dist/')
+						.replace('src/decorators/', 'dist/decorators/')
 						.replace('.ts', '.d.cts'),
 					format: 'cjs',
 				},
 				{
 					file: input
-						.replace('src/', 'dist/')
+						.replace('src/decorators/', 'dist/decorators/')
 						.replace('.ts', '.d.ts'),
 					format: 'esm',
 				},
 			],
-			external: [],
-			plugins: [typescript(), dts({ respectExternal: true })],
+			external: [/^@\/(?!decorators).+/],
+			plugins: [typescript(), dts({ respectExternal: true }), path_reset],
 		};
 		return config;
 	}),
-	...decoratorsBundle,
 ];
