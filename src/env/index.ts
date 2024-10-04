7c65b66577a92e79618ff64c3372cd07c3931ff4
WangYige <1776686515@qq.com>
2024-10-04 11:14:30 +0800

    环境变量声明调整

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,35 +1,23 @@
-import { singleton } from '@/clazz';
-import { isDef } from '@/is';
-
 /**
  * Get the environment variable, default is `zh-CN`,
  * in chrome will use `navigator.language` to get the language in setting.
  */
 export const languageInfo = (() => {
-	const i = singleton(
-		class Language {
-			#env = 'zh-CN';
-
-			constructor() {
-				if (window && window.navigator) {
-					this.#env = window.navigator.language;
-				}
-			}
-
-			get value() {
-				return this.#env;
-			}
-
-			get() {
-				return this.#env;
-			}
-
-			set(value: string) {
-				this.#env = value;
-			}
+	let env = 'zh-CN';
+	if (typeof window !== 'undefined' && window.navigator) {
+		env = window.navigator.language;
+	}
+	return {
+		get value() {
+			return env;
 		},
-	);
-	return new i();
+		get() {
+			return env;
+		},
+		set(value: string) {
+			env = value;
+		},
+	};
 })();
 
 /**
@@ -39,9 +27,9 @@ export const languageInfo = (() => {
  * - if `global` is not defined, use `window`.
  */
 export const globalVar = (() => {
-	if (isDef(globalThis)) {
+	if (typeof globalThis !== 'undefined') {
 		return globalThis;
-	} else if (isDef(global)) {
+	} else if (typeof global !== 'undefined') {
 		return global;
 	}
 	return window;
