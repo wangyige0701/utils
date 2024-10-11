c91235c337e0575aed4561ddd8ae36595c48d759
WangYige <1776686515@qq.com>
2024-10-12 00:01:26 +0800

    调整globalThis获取方法

--- src/useful/debounce/index.ts
+++ src/useful/debounce/index.ts
@@ -6,7 +6,7 @@ import type {
 } from '@/types';
 import { isFunction, isNumber } from '@/is';
 import { createPromise } from '@/promise';
-import { globals } from '@/env';
+import { getGlobal } from '@/env';
 
 type DebounceOptions<Args extends any[]> = {
 	/**
@@ -106,7 +106,7 @@ export function debounce<
 		>();
 
 		// logic
-		timer = globals.setTimeout(async () => {
+		timer = getGlobal().setTimeout(async () => {
 			const result = await useFunc(...(params as P));
 			const funs = callbacks.splice(0);
 			for (let i = 0; i < funs.length; i++) {
